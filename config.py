964f8af915c846782c0c0a9f6644d66474ba683d
Mateusz Wicherski <wicherskimateusz@gmail.com>
2020-08-11 19:10:33 +0200

    Changing color for all bars if sorted

--- config.py
+++ config.py
@@ -3,10 +3,12 @@ HEIGHT = 480
 
 SCREEN_SIZE = (WIDTH, HEIGHT)
 
+# COLORS
 WHITE = (255, 255, 255)
 
 BAR_COLOR = (0, 100, 0)
-
+SELECTED_BAR_COLOR = (0, 0, 100)
+SORTED_BAR_COLOR = (100, 100, 100)
 '''BAR VARIABLES'''
 # How much bars do we want to display and sort
 BARS = 10
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import random
 from config import *
 pygame.init()
 
+# Store all heights in this
 bars = []
+
+# Changing color while selected
+global color
 
 # SCREEN
 screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -15,29 +19,44 @@ def bubleSort(list):
     listLenght = len(list)
     for i in range(listLenght):
         for j in range(0, listLenght-1-i):
-            
             #Drawing
             for k in range(BARS):
+                if bars[k] is bars[j] or bars[k] is bars[j + 1]:
+                    color = SELECTED_BAR_COLOR
+                else:
+                    color = BAR_COLOR
+
                 posX = (k * BAR_WIDTH) + (k * SPACE) + (WIDTH - (BARS * BAR_WIDTH * SPACE)) / 2
                 height = bars[k]
-                drawBars(posX, height)
+                drawBars(posX, height, color)
+            # Updating screen    
             pygame.display.update()
             pygame.time.wait(1000)
+            screen.fill(WHITE)
             # Buble Sort
             if list[j] > list[j + 1]:
                 list[j], list[j + 1] = list[j + 1], list[j] 
-            screen.fill(WHITE)
+    # After change all bar color
+    color = SORTED_BAR_COLOR
+    drawAllBars(bars, color)
+    message = "Sorted thanks for waiting"
 
+def drawAllBars(list, color):
+    for bar in range(BARS):
+        posX = (bar * BAR_WIDTH) + (bar * SPACE) + (WIDTH - (BARS * BAR_WIDTH * SPACE)) / 2
+        height = list[bar]
+        drawBars(posX, height, color)
+        
 
-def drawBars(posX, height):
-    pygame.draw.rect(screen, BAR_COLOR, (posX, 240, BAR_WIDTH, height), 0)
+def drawBars(posX, height, color):
+    pygame.draw.rect(screen, color, (posX, 240, BAR_WIDTH, height), 0)
     bars.append(height)
 
 def setBarsHeight():
     for i in range(BARS):
         height = random.randint(-100, -10)
         posX = (i * BAR_WIDTH) + (i * SPACE) + (WIDTH - (BARS * BAR_WIDTH * SPACE)) / 2
-        drawBars(posX, height)
+        drawBars(posX, height, BAR_COLOR)
 
 setBarsHeight()
 bubleSort(bars)
