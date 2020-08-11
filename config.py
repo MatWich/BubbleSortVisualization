413fde5946abf75d0cf15f130182fdb31576fd6c
Mateusz Wicherski <wicherskimateusz@gmail.com>
2020-08-11 18:18:54 +0200

    Window & drawing rects

---
+++ config.py
@@ -0,0 +1,17 @@
+WIDTH = 600
+HEIGHT = 480
+
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
+WHITE = (255, 255, 255)
+
+BAR_COLOR = (0, 100, 0)
+
+'''BAR VARIABLES'''
+# How much bars do we want to display and sort
+BARS = 10
+
+BAR_WIDTH = 10
+
+# Space between bars
+SPACE = 2
---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+import random
+from config import *
+pygame.init()
+
+# SCREEN
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("Bubble Sort Visualization")
+screen.fill(WHITE)
+
+def drawBars(posX, height):
+    pygame.draw.rect(screen, BAR_COLOR, (posX, 360, BAR_WIDTH, height), 0)
+
+def setBarsHeight():
+    for i in range(BARS):
+        height = random.randint(-100, -10)
+        posX = (i * BAR_WIDTH) * (i * SPACE)
+        drawBars(posX, height)
+
+setBarsHeight()
+# mainLoop
+run = True
+while run:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    pygame.display.update()
